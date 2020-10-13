c9e357c4c536d8e84434352e8d139076fa8b1627
José <afana500@gmail.com>
2020-10-13 18:04:31 +0200

    Añadida la funcion de acceder al activity Nota desde el Main

--- app/src/main/java/com/example/miprimeraappjoseantonio/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.miprimeraappjoseantonio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import  kotlinx.android.synthetic.main.activity_main.*;
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Boton que nos llevara a nuestra activity Nota
+        btnNota.setOnClickListener(){
+            val intent = Intent(this, Nota::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/miprimeraappjoseantonio/Nota.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/Nota.kt
@@ -2,10 +2,17 @@ package com.example.miprimeraappjoseantonio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.make as make1
 
 class Nota : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nota)
+
+
     }
 }
