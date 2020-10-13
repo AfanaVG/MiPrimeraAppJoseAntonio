ec0b0cef94f8242e3933740c0ab3d635b26da8ba
José <afana500@gmail.com>
2020-10-13 20:00:31 +0200

    Metodo validarEmail creado

--- app/src/main/java/com/example/miprimeraappjoseantonio/Correo.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/Correo.kt
@@ -2,12 +2,23 @@ package com.example.miprimeraappjoseantonio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import kotlinx.android.synthetic.main.activity_correo.*
+import java.util.regex.Pattern
 
 class Correo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_correo)
 
+        btnEnviar_Correo.setOnClickListener(){
 
+        }
+    }
+
+
+    private fun validarEmail(correo:String): Boolean {
+        var pattern:Pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(correo).matches();
     }
 }
