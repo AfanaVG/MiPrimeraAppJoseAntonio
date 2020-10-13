5dde5f44c0dc56ccc6dc166486ce89ffd3e2d2f7
José <afana500@gmail.com>
2020-10-13 20:59:08 +0200

    Activity Amigo.kt creada

---
+++ app/src/main/java/com/example/miprimeraappjoseantonio/Amigo.kt
@@ -0,0 +1,15 @@
+package com.example.miprimeraappjoseantonio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+
+class Amigo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_amigo)
+    }
+
+
+}
