5721773d04f7695329340bcb8f3ff0db93301ea2
José <afana500@gmail.com>
2020-10-13 19:53:03 +0200

    Interfaz del activity Correo.kt creada

--- app/src/main/java/com/example/miprimeraappjoseantonio/Correo.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/Correo.kt
@@ -7,5 +7,7 @@ class Correo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_correo)
+
+
     }
 }
