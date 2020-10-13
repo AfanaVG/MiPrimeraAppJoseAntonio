03045fa3328b17b793b0f8783e4c72de1da4c053
José <afana500@gmail.com>
2020-10-13 21:25:25 +0200

    Creada activity Cita

---
+++ app/src/main/java/com/example/miprimeraappjoseantonio/Cita.kt
@@ -0,0 +1,11 @@
+package com.example.miprimeraappjoseantonio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cita : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cita)
+    }
+}
--- app/src/main/java/com/example/miprimeraappjoseantonio/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.miprimeraappjoseantonio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAmigo.setOnClickListener(){
+            val intent = Intent(this, Cita::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
