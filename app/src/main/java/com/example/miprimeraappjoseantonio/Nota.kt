8961a003e560f57e942182d786dfc1182adb2fbb
José <afana500@gmail.com>
2020-10-13 18:15:59 +0200

    Introducida la función del botón añadir nota

--- app/src/main/java/com/example/miprimeraappjoseantonio/Nota.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/Nota.kt
@@ -2,7 +2,9 @@ package com.example.miprimeraappjoseantonio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_nota.*
 import com.google.android.material.snackbar.Snackbar.make as make1
 
 class Nota : AppCompatActivity() {
@@ -13,6 +15,8 @@ class Nota : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nota)
 
-
+    btnNota_Nota.setOnClickListener(){
+        Toast.makeText(this,"Has introducido la nota "+edtxtNota_Nota.text.toString(), Toast.LENGTH_SHORT).show()
+    }
     }
 }
