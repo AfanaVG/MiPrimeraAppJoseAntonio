e202151cdc502c5a056086dd105cfdd00dfb809c
José <afana500@gmail.com>
2020-10-14 15:18:43 +0200

    Cambiado el codigo de introducir nota a un intent

--- app/src/main/java/com/example/miprimeraappjoseantonio/Nota.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/Nota.kt
@@ -22,33 +22,21 @@ class Nota : AppCompatActivity() {
 
         //Nuestro boton llamara al metodo introducirNota()
         btnNota_Nota.setOnClickListener(){
-                newNote()
+            nuevaNota()
             }
 
     }
 
-    //El metodo recibe las variables que controlan la nota minima y maxima
-    private fun introducirNota(maxNota:Int,minNota: Int){
-        //Si la nota no entra en el rango aceptado por la aplicación saltara un mensaje de error. En caso contrario confirmara la nota
-        if (edtxtNota_Nota.text.toString().toInt()> maxNota || edtxtNota_Nota.text.toString().toInt()< minNota){
-            Toast.makeText(this,"La nota debe estar entre "+minNota+" y "+maxNota, Toast.LENGTH_SHORT).show()
-        }else{
-            Toast.makeText(this,"Has introducido la nota "+edtxtNota_Nota.text.toString(), Toast.LENGTH_SHORT).show()
-        }
+    private fun nuevaNota() {
 
-    }
-
-    private fun newNote() {
-        val label = "Title"
-        val main = "This is the body of the note"
         val intent = Intent()
         intent.setAction(NoteIntents.ACTION_CREATE_NOTE)
-            .putExtra(NoteIntents.EXTRA_NAME, label)
-            .putExtra(NoteIntents.EXTRA_TEXT, main)
+            .putExtra(NoteIntents.EXTRA_NAME, edtxtTitulo_Nota.text.toString())
+            .putExtra(NoteIntents.EXTRA_TEXT, edtxtNota_Nota.text.toString())
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
         } else {
-            Toast.makeText(this, "No App Available", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "No existe aplicación de tomar notas", Toast.LENGTH_SHORT).show()
         }
     }
 }
