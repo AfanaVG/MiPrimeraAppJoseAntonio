bb34aea5414048807775d27b9319df68dcd87cab
José <afana500@gmail.com>
2020-10-14 18:47:47 +0200

    Botones con links habilitados con el metodo abrirLink

--- app/src/main/java/com/example/miprimeraappjoseantonio/AcercaDe.kt
+++ app/src/main/java/com/example/miprimeraappjoseantonio/AcercaDe.kt
@@ -1,11 +1,36 @@
 package com.example.miprimeraappjoseantonio
 
-import androidx.appcompat.app.AppCompatActivity
+
+
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_acerca_de.*
 
 class AcercaDe : AppCompatActivity() {
+
+    var twitter = "https://twitter.com/Afanatwit"
+    var gitHub = "https://github.com/AfanaVG"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_acerca_de)
+
+        btnTwitter_AcercaDe.setOnClickListener(){
+            abrirLink(twitter)
+        }
+
+        btnGitHub_AcercaDe.setOnClickListener(){
+            abrirLink(gitHub)
+        }
+    }
+
+    fun abrirLink(link: String){
+
+        val uri = Uri.parse(link)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+
     }
 }
